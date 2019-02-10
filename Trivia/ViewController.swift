08bc4005c08c447057fe2b256249d955189e8208
Fabian <fabiandemoel@gmail.com>
2019-02-10 15:27:30 +0100

    Initial Commit

---
+++ Trivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trivia
+//
+//  Created by Fabian de Moel on 10/02/2019.
+//  Copyright © 2019 Fabian de Moel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
